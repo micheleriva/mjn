bfadfec8fb6c14d0467ede03a441a88632aa8d73
Michele Riva <master@micheleriva.it>
2018-11-05 18:10:27 +0100

    added tests

--- test/mjn.test.ts
+++ test/mjn.test.ts
@@ -29,6 +29,9 @@ test("Testing MJN on a simple object", () => {
   expect(mjn(SimpleObj, "children_ids[4]")).toBeUndefined();
   expect(mjn(SimpleObj, "foo[4]")).toBeUndefined();
   expect(mjn(SimpleObj, "birth.day.k")).toBeUndefined();
+  expect(mjn(SimpleObj, "_")).toBeUndefined();
+  expect(mjn(SimpleObj, "*[]^^!!!")).toBeUndefined();
+  expect(mjn(SimpleObj, "id[20]")).toBeUndefined();
 });
 
 const ComplexObject = {
@@ -55,12 +58,18 @@ const ComplexObject = {
 test("Testing MJN on a complex object", () => {
   expect(mjn(ComplexObject, "foo")).toBeUndefined();
   expect(mjn(ComplexObject, "matrix[2][1][10][100]")).toBeUndefined();
+  expect(
+    mjn(ComplexObject, "matrix[2][1][10][100].foo.bar.baz")
+  ).toBeUndefined();
+  expect(mjn(ComplexObject, "matrix.idonotexist")).toBeUndefined();
   expect(mjn(ComplexObject, "matrix[0][1]")).toBe(2);
   expect(mjn(ComplexObject, "matrix[1][0]")).toBe(4);
+  expect(mjn(ComplexObject, "matrix[1][2]")).toBe(6);
   expect(mjn(ComplexObject, "nested.object.and.useless")).toBe("values");
   expect(mjn(ComplexObject, "nested.object.with.arrays[0].a[0]")).toBe(1);
   expect(mjn(ComplexObject, "nested.object.with.arrays[0].a[4]")).toBe(5);
   expect(mjn(ComplexObject, "nested.object.with.arrays[1].b[1]")).toBe(7);
+  expect(mjn(ComplexObject, "nested.object.with.arrays[1].b[2]")).toBe(8);
   expect(
     mjn(ComplexObject, "nested.object.with.arrays[3].a[0]")
   ).toBeUndefined();
