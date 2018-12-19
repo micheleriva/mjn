2ddc14e6481175454fca01b9795b0519608469c2
Michele Riva <master@micheleriva.it>
2018-12-19 15:20:17 +0100

    added more tests to expand coverage up to 99%

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 > Simple pseudo-monad to check if a value exists in an object.
 
 [![Build Status](https://img.shields.io/travis-ci/micheleriva/mjn.svg?style=flat-square)](https://travis-ci.org/micheleriva/mjn)
+[![Coverage](https://img.shields.io/codecov/c/github/micheleriva/mjn.svg?style=flat-square)](https://codecov.io/gh/micheleriva/mjn)
 [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
 [![blazing fast](https://img.shields.io/badge/speed-blazing%20%F0%9F%94%A5-brightgreen.svg?style=flat-square)](https://img.shields.io/badge/speed-blazing%20%F0%9F%94%A5-brightgreen.svg?style=flat-square)
 
--- test/mjn.test.ts
+++ test/mjn.test.ts
@@ -23,9 +23,12 @@ const SimpleObj = {
 test("Testing MJN on a simple object", () => {
   expect(mjn(SimpleObj, "name.first")).toBe("John");
   expect(mjn(SimpleObj, "name.last")).toBe("Doe");
-  expect(mjn(SimpleObj, "name.middle")).toBe(undefined);
+  expect(mjn(SimpleObj, "name.middle")).toBeUndefined();
   expect(mjn(SimpleObj, "birth.year")).toBe(1950);
   expect(mjn(SimpleObj, "children_ids[1]")).toBe(29381);
+  expect(mjn(SimpleObj, "children_ids[4]")).toBeUndefined();
+  expect(mjn(SimpleObj, "foo[4]")).toBeUndefined();
+  expect(mjn(SimpleObj, "birth.day.k")).toBeUndefined();
 });
 
 const ComplexObject = {
@@ -50,13 +53,15 @@ const ComplexObject = {
 };
 
 test("Testing MJN on a complex object", () => {
+  expect(mjn(ComplexObject, "foo")).toBeUndefined();
+  expect(mjn(ComplexObject, "matrix[2][1][10][100]")).toBeUndefined();
   expect(mjn(ComplexObject, "matrix[0][1]")).toBe(2);
   expect(mjn(ComplexObject, "matrix[1][0]")).toBe(4);
   expect(mjn(ComplexObject, "nested.object.and.useless")).toBe("values");
   expect(mjn(ComplexObject, "nested.object.with.arrays[0].a[0]")).toBe(1);
   expect(mjn(ComplexObject, "nested.object.with.arrays[0].a[4]")).toBe(5);
   expect(mjn(ComplexObject, "nested.object.with.arrays[1].b[1]")).toBe(7);
-  expect(mjn(ComplexObject, "nested.object.with.arrays[3].a[0]")).toBe(
-    undefined
-  );
+  expect(
+    mjn(ComplexObject, "nested.object.with.arrays[3].a[0]")
+  ).toBeUndefined();
 });
